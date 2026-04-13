4730784ac57a59879bc196293496800a66d44eac
inncent <innokentiykim90@gmail.com>
2026-04-13 21:50:42 +0400

    payment-service(task-7): outbox router added to common route

--- payment-service/app/api/routers/common.py
+++ payment-service/app/api/routers/common.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
 from app.api.routers.payments import router as payments_router
+from app.api.routers.outbox_events import router as outbox_router
 
 http_router_v1 = APIRouter(prefix="/api/v1")
 
 
 http_router_v1.include_router(payments_router)
+http_router_v1.include_router(outbox_router)
 
 
 @http_router_v1.get("/ping")
